07b55672dea17d5e35046e2d4e8e733389714980
anandgitacc <anand.akmail@gmail.com>
2021-10-21 00:55:34 +0530

    test1

--- src/org/test/EmployeeDetail.java
+++ src/org/test/EmployeeDetail.java
@@ -4,17 +4,17 @@ package org.test;
 public class EmployeeDetail {
 
 //           method 1
-private void empName1() {
+private void empName() {
 
 System.out.println("Employee name is : Anand");
 
 }
 //  method 2
-private void empId1() {
+private void empId() {
 System.out.println("employee id is : 4521");
 }
 //  method 3
-private void empMob1() {
+private void empMob() {
 
 System.out.println("Employee mobile no is : 8667070832");
 }
@@ -26,9 +26,9 @@ public static void main(String[] args) {
 EmployeeDetail e = new EmployeeDetail();
 
 // method call
-e.empName1();
-e.empId1();
-e.empMob1();
+e.empName();
+e.empId();
+e.empMob();
 
 }
 
