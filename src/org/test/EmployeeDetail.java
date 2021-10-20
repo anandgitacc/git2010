6cc21240449f4899a273702e331b4a9818ffe217
anandgitacc <anand.akmail@gmail.com>
2021-10-21 00:52:01 +0530

    siva test

--- src/org/test/EmployeeDetail.java
+++ src/org/test/EmployeeDetail.java
@@ -1,35 +1,35 @@
 package org.test;
 
-//          class
+//class
 public class EmployeeDetail {
-	
-	//           method 1
-	private void empName() {
-		
-		System.out.println("Employee name is : Anand");
-		
-	}
-		//  method 2
-	private void empId() {
-		System.out.println("employee id is : 4521");
-	}
-		//  method 3
-	private void empMob() {
-		
-		System.out.println("Employee mobile no is : 8667070832");
-	}
-	
-  //  main method 
-	public static void main(String[] args) {
-		
-		//  object
-		EmployeeDetail e = new EmployeeDetail();
-		
-		// method call
-		e.empName();
-		e.empId();
-		e.empMob();
-		
-	}
+
+//           method 1
+private void empName1() {
+
+System.out.println("Employee name is : Anand");
+
+}
+//  method 2
+private void empId1() {
+System.out.println("employee id is : 4521");
+}
+//  method 3
+private void empMob1() {
+
+System.out.println("Employee mobile no is : 8667070832");
+}
+
+//  main method 
+public static void main(String[] args) {
+
+//  object
+EmployeeDetail e = new EmployeeDetail();
+
+// method call
+e.empName1();
+e.empId1();
+e.empMob1();
+
+}
 
 }
